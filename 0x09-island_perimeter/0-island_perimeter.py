69d6c7d957e8167720be01161c35334fe476bdf2
hibaasim <hiba.asim10@gmail.com>
2024-12-06 02:45:10 +0000

    task-0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''module for island perimeter'''
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid.
+
+    Args:
+        grid (list of list of int): the grid representing the island
+
+    Returns:
+        int: the perimeter of the island
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Check the top edge
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                # Check the bottom edge
+                if i == rows-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                # Check the left edge
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                # Check the right edge
+                if j == cols-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+
+    return perimeter
